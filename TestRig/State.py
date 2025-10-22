b36fd0499dff8136cf643fcf47068b03e34d8187
JensLajordMunk <jensmunklajord@gmail.com>
2025-10-22 15:17:04 +0200

    Changes to run position test

--- TestRig/State.py
+++ TestRig/State.py
@@ -3,4 +3,6 @@ import numpy as np
 class State:
     def __init__(self):
         self.foot_locations = np.zeros((3, 4))
-        self.joint_angles = np.zeros((3, 4))
+        self.joint_angles = np.array([[  0.,  0.,  0.,  0.],
+                                 [ 45., 45., 45., 45.],
+                                 [ -45., -45., -45., -45.]])
