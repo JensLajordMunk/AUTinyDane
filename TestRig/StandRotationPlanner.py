79435574a1d120faac71e45715e23b9eddf0b427
jensm <jensmunklajord@gmail.com>
2025-11-13 14:47:38 +0100

    Sending push for larger merge with other branches

--- TestRig/StandRotationPlanner.py
+++ TestRig/StandRotationPlanner.py
@@ -12,44 +12,24 @@ class StandRotaionPlanner:
         self.hardware_interface = HardwareInterface()
         self.command = Command(self.config)
 
+    def changeCheck(self,command,state):
+        if abs(state - command) > 0.0001:
+
+            change = command - state
+
+            max_step = 0.01 / self.config.frequency
+
+            # Limit the velocity change per iteration
+            if change > max_step:
+                scale = max_step / change
+                state = state + change * scale
+            else:
+                state = command
+
+        return state
+
     def run_rotation(self):
-        if abs(self.state.yaw_stand - self.command.yaw_stand) > 0.0001:
+        self.state.yaw_stand = self.changeCheck(self.command.yaw_stand, self.state.yaw_stand)
+        self.state.pitch_stand = self.changeCheck(self.command.pitch_stand, self.state.pitch_stand)
+        self.state.roll_stand = self.changeCheck(self.command.roll_stand, self.state.roll_stand)
 
-            yaw_change = self.command.yaw_stand - self.state.yaw_stand
-
-            max_step = 0.01 / self.config.frequency
-
-            # Limit the velocity change per iteration
-            if yaw_change > max_step:
-                scale = max_step / yaw_change
-                self.state.yaw_stand = self.state.yaw_stand + yaw_change * scale
-            else:
-                self.state.yaw_stand= self.command.yaw_stand
-
-
-        if abs(self.state.pitch_stand - self.command.pitch_stand) > 0.0001:
-
-            yaw_change = self.command.pitch_stand - self.state.pitch_stand
-
-            max_step = 0.01 / self.config.frequency
-
-            # Limit the velocity change per iteration
-            if yaw_change > max_step:
-                scale = max_step / yaw_change
-                self.state.yaw_stand = self.state.yaw_stand + yaw_change * scale
-            else:
-                self.state.yaw_stand= self.command.yaw_stand
-
-
-        if abs(self.state.yaw_stand - self.command.yaw_stand) > 0.0001:
-
-            yaw_change = self.command.yaw_stand - self.state.yaw_stand
-
-            max_step = 0.01 / self.config.frequency
-
-            # Limit the velocity change per iteration
-            if yaw_change > max_step:
-                scale = max_step / yaw_change
-                self.state.yaw_stand = self.state.yaw_stand + yaw_change * scale
-            else:
-                self.state.yaw_stand= self.command.yaw_stand
