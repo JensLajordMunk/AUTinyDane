Made turning possible using yaw and fixed signs in StancePlanner.py

TestRig/Configuration.py
@@ -57,14 +57,15 @@ class RobotConfig:
         self.stancetime = 2
         self.swingtime = self.stancetime / 3  # semi trot gait pattern
         self.step_height = 0.05
-        self.frequency = 75
+        self.frequency = 30
         self.leg_pairs = np.array([[0, 2], [1, 3]])
 
 
 
         # -------------- Max parameters ----------------
-        self.max_velocityX = 0.02
-        self.max_velocityY = 0.06
-        self.max_yaw_rate = 0
+        self.max_velocityX = 0.1
+        self.max_velocityY = 0.1
+        self.max_yaw_rate =  60*np.pi/180 #rad/s
+        self.max_yaw_stand = 0
         self.max_pitch = 0
         self.max_roll = 0

TestRig/GaitPlanner.py
@@ -2,6 +2,7 @@ from SwingPlanner import SwingPlanner
 from StancePlanner import StancePlanner
 from HardwareInterface import HardwareInterface
 from Kinematics import inverse_kinematics
+from Rotation import complete_kinematics
 from Command import Command
 import numpy as np
 import time
@@ -106,10 +107,11 @@ class GaitPlanner:
 
     def trot_cycle_actuated(self):
         if self.state.firstIt:
-            x0, y0, z0, x1, y1, z1 = self.trot(self.state.leg_pair_in_swing[0], 0)
+            x0, y0, z0, x1, y1, z1 = self.trot(0)
             lengthx0 = len(x0)
             lengthx1 = len(x1)
 
+        ################# CHECKING VELOCITY CHANGES IN BOTH DIRECTIONS ######################
         elif abs(self.state.velocityX - self.command.velocityX) > 0.0001 or abs(self.state.velocityY - self.command.velocityY) > 0.0001:
 
             vel_change_x = self.command.velocityX - self.state.velocityX
@@ -127,6 +129,7 @@ class GaitPlanner:
                 self.state.velocityX = self.command.velocityX
                 self.state.velocityY = self.command.velocityY
 
+            ########## CHANGING POSITIONS BASED ON NEW VELOCITY ##############
             if self.state.leg_pair_in_swing[0]:
                 x0, y0, z0 = self.swing_planner.discretizer()
                 lengthx0 = len(x0)
@@ -140,45 +143,97 @@ class GaitPlanner:
                 self.state.legpair_phases_remaining[0] = lengthx0
 
             if self.state.leg_pair_in_swing[1]:
-                x1, y1, z1 = self.gait_planner.swing_planner.discretizer()
+                x1, y1, z1 = self.swing_planner.discretizer()
                 lengthx1 = len(x1)
             else:
                 n = self.state.legpair_phases_remaining[1]
                 currentX1 = self.state.foot_locations[0, 1]
                 currentY1 = self.state.foot_locations[1, 1]
-                x1, y1, z1 = self.gait_planner.stance_planner.linear_discretizer_manual(currentX1, currentY1, n)
+                x1, y1, z1 = self.stance_planner.linear_discretizer_manual(currentX1, currentY1, n)
                 x1, y1, z1 = x1[1:], y1[1:], z1[1:]
                 lengthx1 = len(x1)
                 self.state.legpair_phases_remaining[1] = lengthx1
 
+        ############## CHECKING YAW CHANGE ####################
+        if abs(self.command.trot_yaw-self.state.trot_yaw) > 0.0001 and self.state.leg_pair_in_swing[0] == self.state.leg_pair_in_swing[1]:
+            yaw_change = self.command.trot_yaw-self.state.trot_yaw
+
+            max_step = 0.1 / self.config.frequency
+
+            # Limit the yaw change per iteration
+            if abs(yaw_change) > max_step:
+                scale = max_step / yaw_change
+                self.state.trot_yaw = self.state.trot_yaw + yaw_change * scale
+            else:
+                self.state.trot_yaw = self.command.trot_yaw
+
+        ################# CHECK IF PHASE ENDED ###############3
         if self.state.legpair_phases_remaining[0] == 0:
-            x0, y0, z0 = self.gait_planner.trot(self.state.leg_pair_in_swing[0], 0)
+            x0, y0, z0 = self.trot(0)
             lengthx0 = len(x0)
+            if not self.state.leg_pair_in_swing[1]:
+                self.state.stance_yaw_pair[0] = 0
 
         if self.state.legpair_phases_remaining[1] == 0:
-            x1, y1, z1 = self.gait_planner.trot(self.state.leg_pair_in_swing[1], 1)
+            x1, y1, z1 = self.trot(1)
             lengthx1 = len(x1)
+            if not self.state.leg_pair_in_swing[1]:
+                self.state.stance_yaw_pair[1] = 0
 
+        ####################### ACTUATING THE FIRST LEG PAIR ########################################
+        positions_legpair0 = np.array([x0[lengthx0 - self.state.legpair_phases_remaining[0]],
+                                       y0[lengthx0 - self.state.legpair_phases_remaining[0]],
+                                       z0[lengthx0 - self.state.legpair_phases_remaining[0]]])
+
+        # Changing yaw progressively
+        if self.state.leg_pair_in_swing[0]:
+            rate_swing = self.config.swingtime * self.config.frequency
+            self.state.stance_yaw_pair[0] -= self.state.trot_yaw/rate_swing
+            minimum = min(0, self.state.trot_yaw)
+            maximum = max(0, self.state.trot_yaw)
+            self.state.stance_yaw_pair[0] = np.clip(self.state.stance_yaw_pair[0], minimum, maximum)
+        else:
+            rate_stance = self.config.stancetime * self.config.frequency
+            self.state.stance_yaw_pair[0] += self.state.trot_yaw/rate_stance
+            minimum = min(0, self.state.trot_yaw)
+            maximum = max(0, self.state.trot_yaw)
+            self.state.stance_yaw_pair[0] = np.clip(self.state.stance_yaw_pair[0], minimum, maximum)
+
+        # actuating next position
         for leg_index in self.config.leg_pairs[0, :]:
-            positions_legpair0 = np.array([x0[lengthx0 - self.state.legpair_phases_remaining[0]],
-                                           y0[lengthx0 - self.state.legpair_phases_remaining[0]],
-                                           z0[lengthx0 - self.state.legpair_phases_remaining[0]]])
+            positions_legpair0 = complete_kinematics(positions_legpair0,self.state.stance_yaw_pair[0],0,0,leg_index,self.config)
             current_angles_rad = inverse_kinematics(positions_legpair0, leg_index, self.config)
             for motor_index in range(3):
                 self.hardware_interface.set_actuator_position(current_angles_rad[motor_index], leg_index, motor_index)
                 self.state.foot_locations[motor_index, leg_index] = positions_legpair0[motor_index]
-                self.record_point(leg_index, positions_legpair0)
+
+        ####################### ACTUATING THE SECOND LEG PAIR ########################################
+        positions_legpair1 = np.array([x1[lengthx1 - self.state.legpair_phases_remaining[1]],
+                                       y1[lengthx1 - self.state.legpair_phases_remaining[1]],
+                                       z1[lengthx1 - self.state.legpair_phases_remaining[1]]])
+
+        # Changing yaw progressively
+        if self.state.leg_pair_in_swing[1]:
+            rate_swing = self.config.swingtime * self.config.frequency
+            self.state.stance_yaw_pair[1] -= self.state.trot_yaw/rate_swing
+            minimum = min(0, self.state.trot_yaw)
+            maximum = max(0, self.state.trot_yaw)
+            self.state.stance_yaw_pair[1] = np.clip(self.state.stance_yaw_pair[1], minimum, maximum)
+        else:
+            rate_stance = self.config.stancetime * self.config.frequency
+            self.state.stance_yaw_pair[1] += self.state.trot_yaw/rate_stance
+            minimum = min(0, self.state.trot_yaw)
+            maximum = max(0, self.state.trot_yaw)
+            self.state.stance_yaw_pair[1] = np.clip(self.state.stance_yaw_pair[1], minimum, maximum)
 
         for leg_index in self.config.leg_pairs[1, :]:
-            positions_legpair1 = np.array([x1[lengthx1 - self.state.legpair_phases_remaining[1]],
-                                           y1[lengthx1 - self.state.legpair_phases_remaining[1]],
-                                           z1[lengthx1 - self.state.legpair_phases_remaining[1]]])
+            positions_legpair1 = complete_kinematics(positions_legpair1, self.state.stance_yaw_pair[1], 0, 0, leg_index, self.config)
             current_angles_rad = inverse_kinematics(positions_legpair1, leg_index, self.config)
             for motor_index in range(3):
                 self.hardware_interface.set_actuator_position(current_angles_rad[motor_index], leg_index, motor_index)
                 self.state.foot_locations[motor_index, leg_index] = positions_legpair1[motor_index]
-                self.record_point(leg_index, positions_legpair1)
 
+        ########## PROGRESSING TO NEXT ITERATION ################
         self.state.legpair_phases_remaining[0] -= 1
         self.state.legpair_phases_remaining[1] -= 1
 

TestRig/StancePlanner.py
@@ -14,15 +14,25 @@ class StancePlanner:
 
     def linear_discretizer(self):
         n = max(2,int(self.config.stancetime * self.config.frequency))
-        TDX, TDY = self.touchdown_location()
-        x_uni=np.linspace(TDX,-TDX,n)
-        y_uni = np.linspace(TDY, -TDY, n)
-        z_uni=np.zeros(n) -self.config.body_height
+        if self.state.velocityX + self.state.velocityY == 0:
+            x_uni = np.zeros(n)
+            y_uni = np.zeros(n)
+            z_uni = np.zeros(n) - self.config.body_height
+        else:
+            TDX, TDY = self.touchdown_location()
+            x_uni=np.linspace(TDX,-TDX,n)
+            y_uni = np.linspace(TDY, -TDY, n)
+            z_uni=np.zeros(n) -self.config.body_height
         return x_uni, y_uni, z_uni
 
     def linear_discretizer_manual(self, Xbegin, Ybegin,n):
-        TDX, TDY = self.touchdown_location()
-        x_uni = np.linspace(Xbegin,-TDX,n)
-        y_uni = np.linspace(Ybegin, -TDY, n)
-        z_uni=np.zeros(n) -self.config.body_height
+        if self.state.velocityX + self.state.velocityY == 0:
+            x_uni = np.zeros(n)
+            y_uni = np.zeros(n)
+            z_uni = np.zeros(n) - self.config.body_height
+        else:
+            TDX, TDY = self.touchdown_location()
+            x_uni = np.linspace(Xbegin,-TDX,n)
+            y_uni = np.linspace(Ybegin, -TDY, n)
+            z_uni=np.zeros(n) -self.config.body_height
         return x_uni, y_uni, z_uni

TestRig/SwingPlanner.py
@@ -23,30 +23,12 @@ class SwingPlanner:
         assert self.config.arcR * 2 < self.config.step_height, "The radius is too large compared to the step height"
 
         TDX, TDY = self.touchdown_location()
-
-        signx = 1 if self.state.velocityX >= 0 else -1
-        signy = 1 if self.state.velocityY >= 0 else -1
-        if signx<0 and signy>0:
-            x1 = (-np.sqrt(TDX ** 2 + TDY ** 2) - self.config.arcR * np.sin(self.thetaZ())) * np.cos(self.velAngle())
-            x2 = -self.config.arcR*np.cos(self.velAngle())*np.sin(self.thetaZ())*signy
-            y1 = (-np.sqrt(TDX**2+TDY**2) - self.config.arcR*np.sin(self.thetaZ())) * np.sin(self.velAngle())
-            y2 = -self.config.arcR*np.sin(self.velAngle())*np.sin(self.thetaZ())*signy
-            z1 = self.config.arcR + np.cos(self.thetaZ()) * self.config.arcR
-            z2 = self.config.step_height - self.config.arcR + self.config.arcR*np.cos(self.thetaZ())
-        elif signx>0 and signy<0:
-            x1 = (-np.sqrt(TDX ** 2 + TDY ** 2) - self.config.arcR * np.sin(self.thetaZ())) * np.cos(self.velAngle())
-            x2 = -self.config.arcR*np.cos(self.velAngle())*np.sin(self.thetaZ())*signx
-            y1 = (-np.sqrt(TDX**2+TDY**2) - self.config.arcR*np.sin(self.thetaZ())) * np.sin(self.velAngle())
-            y2 = -self.config.arcR*np.sin(self.velAngle())*np.sin(self.thetaZ())*signx
-            z1 = self.config.arcR + np.cos(self.thetaZ()) * self.config.arcR
-            z2 = self.config.step_height - self.config.arcR + self.config.arcR*np.cos(self.thetaZ())
-        else:
-            x1 = (-np.sqrt(TDX ** 2 + TDY ** 2) - self.config.arcR * np.sin(self.thetaZ())) * np.cos(self.velAngle())
-            x2 = -self.config.arcR*np.cos(self.velAngle())*np.sin(self.thetaZ())
-            y1 = (-np.sqrt(TDX**2+TDY**2) - self.config.arcR*np.sin(self.thetaZ())) * np.sin(self.velAngle())
-            y2 = -self.config.arcR*np.sin(self.velAngle())*np.sin(self.thetaZ())
-            z1 = self.config.arcR + np.cos(self.thetaZ()) * self.config.arcR
-            z2 = self.config.step_height - self.config.arcR + self.config.arcR*np.cos(self.thetaZ())
+        x1 = -TDX - self.config.arcR * np.sin(self.thetaZ()) * round(np.cos(self.velAngle()),10)
+        x2 = -self.config.arcR*np.sin(self.thetaZ()) * round(np.cos(self.velAngle()),10)
+        y1 = -TDY - self.config.arcR*np.sin(self.thetaZ()) * round(np.sin(self.velAngle()),10)
+        y2 = -self.config.arcR*round(np.sin(self.velAngle()),10)*np.sin(self.thetaZ())
+        z1 = self.config.arcR + np.cos(self.thetaZ()) * self.config.arcR
+        z2 = self.config.step_height - self.config.arcR + self.config.arcR*np.cos(self.thetaZ())
         return x1, x2, y1, y2, z1, z2
 
     def bottom_arc_length(self): #Symmetry gives that the two bottom arcs are of equal length
@@ -69,9 +51,6 @@ class SwingPlanner:
         return bottom_arc_time, linear_length_time, top_arc_time
 
     def arc_angles(self):
-        signx = 1 if self.state.velocityX >= 0 else -1
-        signy = 1 if self.state.velocityY >= 0 else -1
-
         angle1_start = -np.pi/2
         angle1_end = np.pi/2 + self.thetaZ()
 
@@ -82,19 +61,6 @@ class SwingPlanner:
         angle3_end = -np.pi / 2
 
         return angle1_start, angle1_end, angle2_start, angle2_end, angle3_start, angle3_end
-
-    def f_lims(self):
-        x1, x2, y1, y2, z1, z2 = self.key_points()
-        lim1 = np.array([x1,x2])
-        assert np.all(np.isfinite(lim1)), "lim1 is not finite"
-        lim2 = np.array([-x2,-x1])
-        assert np.all(np.isfinite(lim2)), "lim2 is not finite"
-        lim3 = np.array([y1,y2])
-        assert np.all(np.isfinite(lim3)), "lim3 is not finite"
-        lim4 = np.array([-y2,-y1])
-        assert np.all(np.isfinite(lim4)), "lim4 is not finite"
-
-        return lim1, lim2, lim3, lim4
 
     def f1(self):
         x1, x2, y1, y2, z1, z2 = self.key_points()
@@ -126,31 +92,43 @@ class SwingPlanner:
         angles = np.linspace(angle_start, angle_end, n)
 
         unirform_arc = self.config.arcR*np.cos(angles)
-
-        x_uni = unirform_arc * np.cos(self.velAngle()) + xcenter
-        y_uni = unirform_arc * np.sin(self.velAngle()) + ycenter
+        x_uni = unirform_arc * round(np.cos(self.velAngle()),10) + xcenter
+        y_uni = unirform_arc * round(np.sin(self.velAngle()),10) + ycenter
         z_uni = self.config.arcR * np.sin(angles) + zcenter
         return x_uni, y_uni, z_uni
 
+    def zero_velocity_discrete(self):
+        n = int(0.5 * self.config.swingtime * self.config.frequency)
+        array_up = np.linspace(0, 1, n)
+        array_down = np.linspace(1, 0, n)
+        smoothed_up = 6 * array_up ** 5 - 15 * array_up ** 4 + 10 * array_up ** 3
+        smoothed_down = 6 * array_down ** 5 - 15 * array_down ** 4 + 10 * array_down ** 3
+        smoothed_motion_up = self.config.step_height * smoothed_up
+        smoothed_motion_down = self.config.step_height * smoothed_down
+        return np.concatenate((smoothed_motion_up, smoothed_motion_down))
+
     def discretizer(self):
+        if self.state.velocityX + self.state.velocityY == 0:
+            z_discrete = self.zero_velocity_discrete() - self.config.body_height
+            x_discrete = np.zeros(len(z_discrete))
+            y_discrete = np.zeros(len(z_discrete))
+        else:
+            bottom_arc_time, linear_length_time, top_arc_time = self.section_phase_times()
+            angle1_start, angle1_end, angle2_start, angle2_end, angle3_start, angle3_end = self.arc_angles()
+            TDX, TDY = self.touchdown_location()
 
-        lim1, lim2, lim3, lim4 = self.f_lims()
-        bottom_arc_time, linear_length_time, top_arc_time = self.section_phase_times()
-        angle1_start, angle1_end, angle2_start, angle2_end, angle3_start, angle3_end = self.arc_angles()
-        TDX, TDY = self.touchdown_location()
+            x1_discretized, y1_discretized, z1_discretized = self.circular_discretizer(angle1_start, angle1_end, -TDX, -TDY, self.config.arcR, bottom_arc_time)
 
-        x1_discretized, y1_discretized, z1_discretized = self.circular_discretizer(angle1_start, angle1_end, -TDX, -TDY, self.config.arcR, bottom_arc_time)
+            x2_discretized, y2_discretized, z2_discretized = self.f1()
 
-        x2_discretized, y2_discretized, z2_discretized = self.f1()
+            x3_discretized, y3_discretized, z3_discretized = self.circular_discretizer(angle2_start, angle2_end, 0, 0, self.config.step_height-self.config.arcR, top_arc_time)
 
-        x3_discretized, y3_discretized, z3_discretized = self.circular_discretizer(angle2_start, angle2_end, 0, 0, self.config.step_height-self.config.arcR, top_arc_time)
+            x4_discretized, y4_discretized, z4_discretized = self.f2()
 
-        x4_discretized, y4_discretized, z4_discretized = self.f2()
+            x5_discretized, y5_discretized, z5_discretized = self.circular_discretizer(angle3_start, angle3_end, TDX, TDY, self.config.arcR, bottom_arc_time)
 
-        x5_discretized, y5_discretized, z5_discretized = self.circular_discretizer(angle3_start, angle3_end, TDX, TDY, self.config.arcR, bottom_arc_time)
-
-        x_discrete = np.concatenate([x1_discretized, x2_discretized, x3_discretized, x4_discretized, x5_discretized])
-        y_discrete = np.concatenate([y1_discretized, y2_discretized, y3_discretized, y4_discretized, y5_discretized])
-        z_discrete = np.concatenate([z1_discretized, z2_discretized, z3_discretized, z4_discretized, z5_discretized]) - self.config.body_height
+            x_discrete = np.concatenate([x1_discretized, x2_discretized, x3_discretized, x4_discretized, x5_discretized])
+            y_discrete = np.concatenate([y1_discretized, y2_discretized, y3_discretized, y4_discretized, y5_discretized])
+            z_discrete = np.concatenate([z1_discretized, z2_discretized, z3_discretized, z4_discretized, z5_discretized]) - self.config.body_height
 
         return x_discrete, y_discrete, z_discrete

TestRig/gait_simulator.py
@@ -6,6 +6,7 @@ from matplotlib.animation import FuncAnimation
 from Configuration import RobotConfig
 from State import State
 from Kinematics import inverse_kinematics
+from Rotation import complete_kinematics
 from Command import Command
 import time
 import sys
@@ -85,7 +86,7 @@ class GaitSimulator:
                 vel_change_y = self.command.velocityY - self.state.velocityY
                 vel_change_magnitude = np.sqrt(vel_change_x ** 2 + vel_change_y ** 2)
 
-                max_step = 0.01 / self.config.frequency
+                max_step = 0.05 / self.config.frequency
 
                 # Limit the velocity change per iteration
                 if vel_change_magnitude > max_step:
@@ -120,25 +121,77 @@ class GaitSimulator:
                     lengthx1 = len(x1)
                     self.state.legpair_phases_remaining[1] = lengthx1
 
+            if abs(self.command.trot_yaw - self.state.trot_yaw) > 0.001 and self.state.leg_pair_in_swing[0] == self.state.leg_pair_in_swing[1]:
+                yaw_change = self.command.trot_yaw - self.state.trot_yaw
+
+                max_step = 10 / self.config.frequency
+
+                # Limit the yaw change per iteration
+                if abs(yaw_change) > max_step:
+                    scale = max_step / yaw_change
+                    self.state.trot_yaw = self.state.trot_yaw + yaw_change * scale
+                else:
+                    self.state.trot_yaw = self.command.trot_yaw
 
             if self.state.legpair_phases_remaining[0] == 0:
                 x0, y0, z0 = self.gait_planner.trot(0)
+                if not self.state.leg_pair_in_swing[0]:
+                    self.state.stance_yaw_pair[0] = 0
                 lengthx0 = len(x0)
 
             if self.state.legpair_phases_remaining[1] == 0:
                 x1, y1, z1 = self.gait_planner.trot(1)
+                if not self.state.leg_pair_in_swing[1]:
+                    self.state.stance_yaw_pair[1] = 0
                 lengthx1 = len(x1)
 
+            positions_legpair0 = np.array([x0[lengthx0 - self.state.legpair_phases_remaining[0]],
+                                           y0[lengthx0 - self.state.legpair_phases_remaining[0]],
+                                           z0[lengthx0 - self.state.legpair_phases_remaining[0]]])
+
+            # Changing yaw progressively
+            if self.state.leg_pair_in_swing[0]:
+                rate_swing = self.config.swingtime * self.config.frequency
+                self.state.stance_yaw_pair[0] -= self.state.trot_yaw / rate_swing
+                minimum = min(0, self.state.trot_yaw)
+                maximum = max(0, self.state.trot_yaw)
+                self.state.stance_yaw_pair[0] = np.clip(self.state.stance_yaw_pair[0],minimum,maximum)
+            else:
+                rate_stance = self.config.stancetime * self.config.frequency
+                self.state.stance_yaw_pair[0] += self.state.trot_yaw / rate_stance
+                minimum = min(0, self.state.trot_yaw)
+                maximum = max(0, self.state.trot_yaw)
+                self.state.stance_yaw_pair[0] = np.clip(self.state.stance_yaw_pair[0],minimum,maximum)
+
             for leg_index in self.config.leg_pairs[0, :]:
-                positions_legpair0 = np.array([x0[lengthx0 - self.state.legpair_phases_remaining[0]], y0[lengthx0 - self.state.legpair_phases_remaining[0]], z0[lengthx0 - self.state.legpair_phases_remaining[0]]])
+                positions_legpair0 = complete_kinematics(positions_legpair0, self.state.stance_yaw_pair[0], 0, 0, leg_index, self.config)
                 current_angles_rad = inverse_kinematics(positions_legpair0, leg_index, self.config)
                 for motor_index in range(3):
                     self.hardware_interface.set_actuator_position(current_angles_rad[motor_index], leg_index, motor_index)
                     self.state.foot_locations[motor_index, leg_index] = positions_legpair0[motor_index]
                     self.record_point(leg_index, positions_legpair0)
 
+
+            positions_legpair1 = np.array([x1[lengthx1 - self.state.legpair_phases_remaining[1]],
+                                           y1[lengthx1 - self.state.legpair_phases_remaining[1]],
+                                           z1[lengthx1 - self.state.legpair_phases_remaining[1]]])
+
+            # Changing yaw progressively
+            if self.state.leg_pair_in_swing[1]:
+                rate_swing = self.config.swingtime * self.config.frequency
+                self.state.stance_yaw_pair[1] -= self.state.trot_yaw / rate_swing
+                minimum = min(0, self.state.trot_yaw)
+                maximum = max(0, self.state.trot_yaw)
+                self.state.stance_yaw_pair[1] = np.clip(self.state.stance_yaw_pair[1],minimum,maximum)
+            else:
+                rate_stance = self.config.stancetime * self.config.frequency
+                self.state.stance_yaw_pair[1] += self.state.trot_yaw / rate_stance
+                minimum = min(0, self.state.trot_yaw)
+                maximum = max(0, self.state.trot_yaw)
+                self.state.stance_yaw_pair[1] = np.clip(self.state.stance_yaw_pair[1],minimum,maximum)
+
             for leg_index in self.config.leg_pairs[1, :]:
-                positions_legpair1 = np.array([x1[lengthx1 - self.state.legpair_phases_remaining[1]], y1[lengthx1 - self.state.legpair_phases_remaining[1]], z1[lengthx1 - self.state.legpair_phases_remaining[1]]])
+                positions_legpair1 = complete_kinematics(positions_legpair1, self.state.stance_yaw_pair[1], 0, 0, leg_index, self.config)
                 current_angles_rad = inverse_kinematics(positions_legpair1, leg_index, self.config)
                 for motor_index in range(3):
                     self.hardware_interface.set_actuator_position(current_angles_rad[motor_index], leg_index, motor_index)
@@ -148,12 +201,13 @@ class GaitSimulator:
             self.state.legpair_phases_remaining[0] -= 1
             self.state.legpair_phases_remaining[1] -= 1
 
-            if iter==200 or iter == 400:
-                self.command.L3[1] += 2
-            if iter%1 == 0:
-                self.command.L3[1] -= 0.01
+            #if iter==200 or iter == 400:
+            #    self.command.L3[1] += 2
+            if 0<iter<100:
+                self.command.R3[1] = 1
+            else:
+                self.command.R3[1] = 0
             iter+=1
-
             time.sleep((1.0 / self.config.frequency) - (time.time()-loop_time))
 
         return self.front_left, self.front_right
@@ -165,8 +219,6 @@ fl = np.asarray(fl)
 fr = np.asarray(fr)
 
 def test_swing_planner_animated():
-    config = RobotConfig()
-
     fl_x = fl[:, 0]
     fl_y = fl[:, 1]
     fl_z = fl[:, 2]
@@ -486,6 +538,6 @@ def animate_foot_3d(foot_data, foot_name='Front Left'):
 
     return anim
 
-plot_front_leg_joint_angles()
+#plot_front_leg_joint_angles()
 #test_swing_planner_animated()
-#animate_foot_3d(fr, 'Front Right')
+animate_foot_3d(fr, 'Front Right')