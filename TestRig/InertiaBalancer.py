3852305977223f7a7642418985c0f9514c05a20f
jensm <jensmunklajord@gmail.com>
2025-11-25 13:50:13 +0100

    Updated to respect max acceleration

--- TestRig/InertiaBalancer.py
+++ TestRig/InertiaBalancer.py
@@ -8,9 +8,9 @@ class InertiaBalancer:
         self.config = config
 
         self.kp_pitch = 10
-        self.kp_roll = 0.05
+        self.kp_roll = 10
 
-        self.kd_pitch = 10
+        self.kd_pitch = 0.05
         self.kd_roll = 0.002
 
     def velocity_offset(self, dt, pitch, roll, gx, gy):
@@ -18,6 +18,10 @@ class InertiaBalancer:
 
         accel_x = -(self.kp_pitch * pitch) - (self.kd_pitch * gy)
         accel_y = -(self.kp_roll * roll) - (self.kd_roll * gx)
+
+        accel_x = np.clip(accel_x, -self.config.max_acceleration,self.config.max_acceleration)
+        accel_y = np.clip(accel_y, -self.config.max_acceleration,self.config.max_acceleration)
+
         vx_offset = dt * accel_x
         vy_offset = dt * accel_y
 
