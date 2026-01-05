779bac7a27a3be71f6ce0b42e1ecd517fac0cac5
JensLajordMunk <jensmunklajord@gmail.com>
2026-01-05 13:03:46 +0100

    Add max translation change for stand position updates
    Implement gradual translation changes for stand position.

--- src/StandTranslationPlanner.py
+++ src/StandTranslationPlanner.py
@@ -1,7 +1,7 @@
 from src.HardwareInterface import HardwareInterface
 from src.Kinematics import inverse_kinematics
 from src.Rotation import orientation_kinematics
-
+import numpy as np
 
 class StandTranslationPlanner:
 
@@ -10,18 +10,33 @@ class StandTranslationPlanner:
         self.command = command
         self.state = state
         self.hardware_interface = HardwareInterface()
+        self.max_translation_change = 0.001
 
     def run_translation(self):
-        self.state.stand_x =self.command.stand_x
-        self.state.stand_y =self.command.stand_y
-        self.state.stand_z =self.command.stand_z
 
-        #roll_imu, pitch_imu, gx, gy = self.hardware_interface.get_imu_tilt()
+        diff_x = self.command.stand_x - self.state.stand_x
 
-        self.state.stand_pitch -= 0 #pitch_imu
-        self.state.stand_roll -= 0 #roll_imu
+        if abs(diff_x) > self.max_translation_change:
+            self.state.stand_x += np.sign(diff_x) * self.max_translation_change
+        else:
+            self.state.stand_x = self.command.stand_x
 
-        #print(self.state.stand_pitch, self.state.stand_roll)
+
+        diff_y = self.command.stand_y - self.state.stand_y
+
+        if abs(diff_y) > self.max_translation_change:
+            self.state.stand_y += np.sign(diff_y) * self.max_translation_change
+        else:
+            self.state.stand_y = self.command.stand_y
+
+
+        diff_z = self.command.stand_z - self.state.stand_z
+
+        if abs(diff_z) > self.max_translation_change:
+            self.state.stand_z += np.sign(diff_z) * self.max_translation_change
+        else:
+            self.state.stand_z = self.command.stand_z
+
 
         for leg_index in range(4):
             pos = orientation_kinematics([-self.state.stand_x, self.state.stand_y + self.config.abduction_offsets[leg_index], - self.state.stand_z - self.config.body_height], self.state.stand_yaw, self.state.stand_pitch, self.state.stand_roll, leg_index, self.config)
